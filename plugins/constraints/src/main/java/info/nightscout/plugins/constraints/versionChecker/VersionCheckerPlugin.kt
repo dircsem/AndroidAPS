4fe031b8d2e5b5c5b49a18c5fb1b8d608d9159c1
Djuric <dirceu.semighini@protonmail.com>
2024-01-28 15:00:38 -0300

    Update VersionCheckerPlugin.kt

--- plugins/constraints/src/main/java/info/nightscout/plugins/constraints/versionChecker/VersionCheckerPlugin.kt
+++ plugins/constraints/src/main/java/info/nightscout/plugins/constraints/versionChecker/VersionCheckerPlugin.kt
@@ -64,10 +64,10 @@ class VersionCheckerPlugin @Inject constructor(
         checkWarning()
         versionCheckerUtils.triggerCheckVersion()
         if (lastCheckOlderThan(gracePeriod.veryOld.daysToMillis()))
-            value.set(aapsLogger, false, rh.gs(R.string.very_old_version), this)
+            value.set(aapsLogger, true, rh.gs(R.string.very_old_version), this)
         val endDate = sp.getLong(rh.gs(info.nightscout.core.utils.R.string.key_app_expiration) + "_" + config.VERSION_NAME, 0)
         if (endDate != 0L && dateUtil.now() > endDate)
-            value.set(aapsLogger, false, rh.gs(R.string.application_expired), this)
+            value.set(aapsLogger, true, rh.gs(R.string.application_expired), this)
         return value
     }
 
